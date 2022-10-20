623e5f97657bc6d4ea62e3dd7b2b283b9027ac47
Inga Kozlowska <inga@snowschool.pl>
2022-10-20 13:23:26 +0200

    col fixed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ export const Header = (props) => {
         <div className='overlay'>
           <div className='container'>
             <div className='row'>
-              <div className='col-xs-12 col-md-12 col-md-offset-2 intro-text'>
+              <div className='col-md-8 col-md-offset-2 intro-text'>
                 <h1>
                   {props.data ? props.data.title : 'Loading'}
                   <span></span>
